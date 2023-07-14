c2840880278ac646f25818001eca42b186dc2f45
Briton Otieno <ojwangbriton@gmail.com>
2023-07-14 11:41:16 +0300

    Remove commented code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Calculator from './components/Calculator';
 
 const App = () => (
   <div className="app">
-    {/* <h1 className="center">My Calculator</h1> */}
     <Calculator />
   </div>
 );
