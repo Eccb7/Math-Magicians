e62c6ac9350c0877033f7b7f5075a6180e5ef40d
Briton Otieno <ojwangbriton@gmail.com>
2023-07-14 21:28:43 +0300

    Add imports and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
 import React from 'react';
+import './App.css';
 import Calculator from './components/Calculator';
+import QuoteDisplay from './components/quote';
 
 const App = () => (
-  <div className="app">
+  <div className="app-container">
+    <h1>Quote</h1>
+    <QuoteDisplay />
     <Calculator />
   </div>
 );
