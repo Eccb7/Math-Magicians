da03064ba6f10098158fe28c5659d4a3ed236343
Briton Otieno <ojwangbriton@gmail.com>
2023-07-20 14:30:39 +0300

    Add and style the home page

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,118 +15,122 @@ const Calculator = () => {
   };
 
   return (
-    <div className="calculator-container">
-      <div className="calculator">
-        <input type="text" className="result-input w-100" value={calcData.next || calcData.total || ''} readOnly />
-        <div className="keypad">
-          <div className="row">
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('AC')}>
-                AC
-              </button>
+    <>
+      <div className="math">LETS DO SOME MATH</div>
+      <div className="calculator-container">
+        <div className="calculator">
+          <input type="text" className="result-input w-100" value={calcData.next || calcData.total || ''} readOnly />
+          <div className="keypad">
+            <div className="row">
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('AC')}>
+                  AC
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('+/-')}>
+                  +/-
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('%')}>
+                  %
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('÷')}>
+                  /
+                </button>
+              </div>
             </div>
-            <div className="col">
-              <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('+/-')}>
-                +/-
-              </button>
+            <div className="row">
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('7')}>
+                  7
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('8')}>
+                  8
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('9')}>
+                  9
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('-')}>
+                  -
+                </button>
+              </div>
             </div>
-            <div className="col">
-              <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('%')}>
-                %
-              </button>
+            <div className="row">
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('4')}>
+                  4
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('5')}>
+                  5
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('6')}>
+                  6
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('x')}>
+                  x
+                </button>
+              </div>
             </div>
-            <div className="col">
-              <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('÷')}>
-                /
-              </button>
+            <div className="row">
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('1')}>
+                  1
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('2')}>
+                  2
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('3')}>
+                  3
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('+')}>
+                  +
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('7')}>
-                7
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('8')}>
-                8
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('9')}>
-                9
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('-')}>
-                -
-              </button>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('4')}>
-                4
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('5')}>
-                5
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('6')}>
-                6
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('x')}>
-                x
-              </button>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('1')}>
-                1
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('2')}>
-                2
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('3')}>
-                3
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('+')}>
-                +
-              </button>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('0')}>
-                0
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('.')}>
-                .
-              </button>
-            </div>
-            <div className="col">
-              <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('=')}>
-                =
-              </button>
+            <div className="row">
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('0')}>
+                  0
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-gray w-100" onClick={() => handleButtonClick('.')}>
+                  .
+                </button>
+              </div>
+              <div className="col">
+                <button type="button" className="button-orange w-100" onClick={() => handleButtonClick('=')}>
+                  =
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+    </>
   );
 };
 
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => (
+  <h2>
+    WELCOME MATHEMATICIAN.
+    <>
+      <p className="home-p">
+        This is an app wholly to help you with your calculation.
+        Ready? Then get to the next page.
+
+      </p>
+    </>
+  </h2>
+);
+
+export default Home;
