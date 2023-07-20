ad8031993fb3af0022359f86430b9f8477772182
Briton Otieno <ojwangbriton@gmail.com>
2023-07-20 14:31:22 +0300

    Add the route navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import QuoteDisplay from './components/quote';
+import Home from './components/Home';
+
+const Navigation = () => (
+  <nav className="navbar">
+    <div className="navbar-logo">Math-Magicians</div>
+    <ul className="navbar-links">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/calculator">Calculator</Link>
+      </li>
+      <li>
+        <Link to="/quote">Quote</Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 const App = () => (
-  <div className="app-container">
-    <QuoteDisplay />
-    <Calculator />
-  </div>
+  <Router>
+    <div className="app-container">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<QuoteDisplay />} />
+      </Routes>
+    </div>
+  </Router>
 );
 
 export default App;
