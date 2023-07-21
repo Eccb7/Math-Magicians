aa2997b7b91dd2ddd7d4af6d59515e460953f33f
haben mehar <habenmehari71@gmail.com>
2023-07-21 11:07:55 +0300

    test case for numbers added

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -11,3 +11,14 @@ describe('test cases for symbols', () => {
     expect(calculate(data, 'AC')).toEqual({ total: null, next: null, operation: null });
   });
 });
+
+describe('test cases for numbers', () => {
+  test('should return empty object if next is 0 and button name is 0', () => {
+    const data = {
+      total: 0,
+      next: 0,
+      operation: '+',
+    };
+    expect(calculate(data, 0)).toEqual({});
+  });
+});
